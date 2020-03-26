6c6a0b92103a49fd388cd17ba413c88782caab34
Vera Crabtree <veracrabtree@Veras-MacBook-Pro.local>
2020-03-26 17:47:29 -0400

    Tests for validateCredentials

--- src/test/java/TestClassTest.java
+++ src/test/java/TestClassTest.java
@@ -8,4 +8,21 @@ class TestClassTest {
     public void testHello(){
         assertEquals("Hello", TestClass.printHello());
     }
+
+    @Test
+    public void testValidateCredentials() throws AccountDoesNotExistException {
+        // Create Manager Account
+        Manager manager1 = new Manager("manager@shelter.com", "password");
+
+        // Incorrect Password for Manager Account
+        assertFalse(manager1.validateCredentials("manager@shelter.com", "")); // no password
+        assertFalse(manager1.validateCredentials("manager@shelter.com", "wrong")); // random password
+        assertFalse(manager1.validateCredentials("manager@shelter.com", "pasword")); // close to correct password
+
+        // Correct Password for Manager Account
+        assertTrue(manager1.validateCredentials("manager@shelter.com", "password"));
+
+        // Test Account That Does Not Exist
+        assertThrows(AccountDoesNotExistException.class, () -> manager1.validateCredentials("incorrect@shelter.com", "password"));
+    }
 }
