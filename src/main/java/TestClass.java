ff8060543d6b966fbe2d886ce1b3c2b176dc9db5
Vera Crabtree <veracrabtree@Veras-MacBook-Pro.local>
2020-03-26 18:49:51 -0400

    Started validateCredentials method

--- src/main/java/TestClass.java
+++ src/main/java/TestClass.java
@@ -2,4 +2,17 @@ public class TestClass {
     public static String printHello(){
         return("Hello");
     }
+
+    public boolean validateCredentials(String email, String password) throws AccountDoesNotExistException {
+        // If there is no manager with given email {
+            throw new AccountDoesNotExistException("This account does not exists");
+        }
+        // If the manager's password equals the inputted password {
+            return true;
+        }
+        else {
+            return false;
+        }
+        //TODO add file for AccountDoesNotExistException
+    }
 }
