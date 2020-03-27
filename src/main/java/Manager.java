e0d9a6b3b0d62144c3dd3cc04f61bc2ee0c9ccbb
Mariama <mdiallo@ithaca.edu>
2020-03-27 10:46:59 -0400

    Manager accounts

---
+++ src/main/java/Manager.java
@@ -0,0 +1,26 @@
+
+public class Manager {
+    private String id;
+    private String password;
+
+
+    public Manager( String idM,  String passwordM){
+        id = idM;
+        password = passwordM;
+
+    }
+
+
+
+    public String getId() {
+        return id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+
+
+
+}
