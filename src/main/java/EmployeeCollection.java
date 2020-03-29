b656e971c22db89c09dca19abd3a7efbcf165ecc
AidanD-boot <adebolt@ithaca.edu>
2020-03-29 19:04:33 -0400

    Updated Manager Class

--- src/main/java/EmployeeCollection.java
+++ src/main/java/EmployeeCollection.java
@@ -7,9 +7,12 @@ public class EmployeeCollection {
         managers = new HashMap<>();
     }
 
-    public void addManager(String idM, String passwordM){
+    public void addManager(String idM, String passwordM) throws Exception{
         if(managers.get(idM)!= null){
             throw new IllegalArgumentException("Manger Already Exists");
+        }
+        else if(managers.size() >= 1){
+            throw new LimitException("Manager count limit has been reached");
         }
         else{
             managers.put(idM, new Manager(idM, passwordM));
--- src/test/java/EmployeeCollectionTest.java
+++ src/test/java/EmployeeCollectionTest.java
@@ -5,21 +5,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class EmployeeCollectionTest {
 
     @Test
-    void addManager() {
+    void addManager() throws Exception{
         EmployeeCollection mc = new EmployeeCollection();
         assertEquals(0, mc.getManagerCount());
         mc.addManager("Amadou", "yes123");
-        assertEquals(1, mc.getManagerCount());
-        mc.addManager("Kris", "no123");
-        assertEquals(2, mc.getManagerCount());
-        mc.addManager("Ritz", "ok123");
-        assertEquals(3, mc.getManagerCount());
 
+        assertEquals(1, mc.getManagerCount());
+        assertThrows(LimitException.class, ()-> mc.addManager("Kris","no123"));
         assertThrows(IllegalArgumentException.class, ()-> mc.addManager("Amadou", "yes123"));
     }
 
     @Test
-    void deleteManagerAcct() {
+    void deleteManagerAcct() throws Exception {
         EmployeeCollection mc = new EmployeeCollection();
         mc.addManager("Amadou", "yes123");
         mc.addManager("Kris", "no123");
