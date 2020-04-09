3a8cc8249c68bf7a606e6aaad1381182f067ba9d
upstate-online <cedward1@ithaca.edu>
2020-04-09 16:28:52 -0400

    ToDo Structure with Task Objects

---
+++ src/main/java/Task.java
@@ -0,0 +1,13 @@
+public class Task {
+    int priority = 0;
+    String task;
+
+    public Task(int priorityIn, String taskIn){
+        this.priority = priorityIn;
+        this.task = taskIn;
+
+    }
+
+
+
+}
---
+++ src/main/java/TodoList.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class TodoList {
+
+        int taskCount;
+        List<Task> toDoList;
+
+
+        public TodoList(){
+            this.taskCount = 0;
+            this.toDoList = new ArrayList<>();
+
+        }
+}
