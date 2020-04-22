eb35f1ebd539a6f1c27f26c34aad36ba58993546
Matthew Becker <mbecker5@ithaca.edu>
2020-04-21 23:03:13 -0400

    changed a few things in todolist for UI made test file and completed the base of the UI

--- src/main/java/ToDoListAPI.java
+++ src/main/java/ToDoListAPI.java
@@ -1,2 +1,19 @@
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import java.util.PriorityQueue;
+
 public interface ToDoListAPI {
+
+    String check();
+
+    void addTask(Task taskIn);
+
+    void removeTask(String taskToRemove);
+
+    void updateTask(String taskToUpdate, String newTaskName, int newPriority) throws IOException;
+
+    String getToDoList();
+
+    void fromJson(List listin);
 }
--- src/main/java/ToDoListUI.java
+++ src/main/java/ToDoListUI.java
@@ -1,84 +1,93 @@
 import java.util.Scanner;
 
 public class ToDoListUI {
-   // private ToDoListAPI toDoListAPI;
+    private ToDoListAPI toDoListAPI;
     private Scanner read = new Scanner(System.in);
 
-    //public toDoListUI(ToDoListAPI toDoListAPI){
-    //        this.toDoListAPI = toDoListAPI;
-    //    }
+    public ToDoListUI(ToDoListAPI toDoListAPI){
+        this.toDoListAPI = toDoListAPI;
+    }
+
+    public void toDoListAccessed(){
+        read.useDelimiter("\\n");
+        String action;
+        String task;
+        int priority;
+
+        do {
+            System.out.println("Please enter an action or type 'help' for a list of commands");
+            action = read.next();
+            action = action.toLowerCase();
+            try {
+                switch (action) {
+                    case "help":
+                        System.out.println("Your options are: " +
+                                "\n check " +
+                                "\n update " +
+                                "\n add task" +
+                                "\n remove task" +
+                                "\n exit \n");
+                        break;
+
+                    case "check":
+                        toDoListAPI.check();
+                        break;
+
+                    case "update":
+                        System.out.println("Enter task to update");
+                        task = read.next();
+                        String taskToUpdate = task;
+
+                        System.out.println("Enter the updated task");
+                        task = read.next();
+                        String taskUpdated = task;
+
+                        System.out.println("Enter the updated priority");
+                        priority = read.nextInt();
+                        int newPriority = priority;
+
+                        toDoListAPI.updateTask(task, taskUpdated, newPriority);
+                        System.out.println("Task updated");
+                        break;
+
+                    case "add task":
+                        System.out.println("Enter task to add");
+                        task = read.next();
+                        System.out.println("Enter task priority");
+                        priority = read.nextInt();
+
+                        Task newTask = new Task(priority, task);
+
+                        toDoListAPI.addTask(newTask);
+                        System.out.print("The task has been added");
+
+                    case "remove task":
+                        System.out.println("Enter a task to remove");
+                        task = read.next();
+
+                        toDoListAPI.removeTask(task);
+                        System.out.println("The task has been removed");
+                        break;
 
 
-        public void toDoListAccessed(){
-            read.useDelimiter("\\n");
-            String action;
-            String task;
-            int priority;
+                    case "exit":
+                        System.out.println("Thanks for checking the tasks");
+                        break;
 
-            do {
-                System.out.println("Please enter an action or type 'help' for a list of commands");
-                action = read.next();
-                action = action.toLowerCase();
-                try{
-                    switch (action) {
-                        case "help":
-                            System.out.println("Your options are: " +
-                                    "\n check " +
-                                    "\n update " +
-                                    "\n add task" +
-                                    "\n remove task" +
-                                    "\n exit \n");
-                            break;
+                    default:
+                        System.out.println("Please enter a valid input");
 
-                        case "check":
-                            ToDoListAPI.check();
-                            break;
-
-
-                        case "update":
-                            System.out.println("Enter task to update");
-                            task = read.next();
-                            ToDoListAPI.update(task);
-                            System.out.println("Task updated");
-                            break;
-
-                        case "add task":
-                            System.out.println("Enter task task to add");
-                            String task = read.next();
-                            System.out.println("Enter task priority");
-                            priority = read.nextInt();
-                            ToDoListAPI.add(priority, task);
-                            System.out.println("The task has been added");
-
-                        case "remove task":
-                            System.out.println("Enter a task to remove");
-                            task = read.next();
-                            ToDoListAPI.remove(task);
-                            System.out.println("The task has been removed");
-                            break;
-
-
-                        case "exit":
-                            System.out.println("Thanks for checking the tasks");
-                            break;
-
-                        default:
-                            System.out.println("Please enter a valid input");
-
-
-                    }
                 }
-                //catch(AccountFrozenException e){
-                  //  System.out.println(e.getMessage());
-                //}
-                //catch(Exception e){
-                  //  System.out.println(e.getMessage());
-                //}
+            }
+            catch(Exception e){
+
+            }
 
 
-            } while (!action.equals("exit"));
+        } while (!action.equals("exit"));
             toDoListAccessed();
 
-        }
+
     }
 }
+
