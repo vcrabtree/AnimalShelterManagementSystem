14859e61185ad709855cb482ca7914c0f052b6d9
Matthew Becker <mbecker5@ithaca.edu>
2020-05-03 14:39:49 -0400

    added most of UI and API structure

--- src/main/java/AnimalShelter/VolunteerAPI.java
+++ src/main/java/AnimalShelter/VolunteerAPI.java
@@ -1,4 +1,8 @@
-package AnimalShelter;
+import AnimalShelter.Animal;
+import AnimalShelter.AnimalList;
 
 public interface VolunteerAPI {
+    public String checkStatus(int id, AnimalList listIn);
+
+    public void removeTask(String taskToRemove);
 }
